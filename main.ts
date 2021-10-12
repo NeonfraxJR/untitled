21ee794d53c203f569aa0e255e11c52e6964e396
NeonfraxJR <91573755+NeonfraxJR@users.noreply.github.com>
2021-10-12 11:57:52 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showString("heeeeeeelllllllllllpppppppppppp")
+basic.forever(function () {
+    pins.setAudioPin(AnalogPin.P0)
+    music.setVolume(127)
+    soundExpression.giggle.playUntilDone()
+})
