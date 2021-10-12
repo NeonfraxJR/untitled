e83a8255b37eaca1f749196529379b5eb7035af0
NeonfraxJR <91573755+NeonfraxJR@users.noreply.github.com>
2021-10-12 11:58:57 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
 basic.showString("heeeeeeelllllllllllpppppppppppp")
 basic.forever(function () {
-    pins.setAudioPin(AnalogPin.P0)
-    music.setVolume(127)
-    soundExpression.giggle.playUntilDone()
+	
 })
